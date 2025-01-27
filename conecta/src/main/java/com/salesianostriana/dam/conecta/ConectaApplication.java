da7005610c38dc111de03786e8ad44e304b02373
ppsegur <pepesegurarodriguez@gmail.com>
2025-01-27 11:40:58 +0100

    Subida del proyecto conecta con las prncipales dependencias

---
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/ConectaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.conecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaApplication.class, args);
+	}
+
+}
---
+++ conecta/src/test/java/com/salesianostriana/dam/conecta/ConectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.conecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
