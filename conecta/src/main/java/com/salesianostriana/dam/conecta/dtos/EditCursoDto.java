d018f06e728118fdcb2e38517d94d31adc493506
ppsegur <pepesegurarodriguez@gmail.com>
2025-01-28 10:59:58 +0100

    Inicio de servicio de curso además del dto del curso

---
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/EditCursoDto.java
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.conecta.dtos;
+
+import com.salesianostriana.dam.conecta.model.Curso;
+
+//Dto Curso
+public record EditCursoDto(String nombre, int horasEmpresa) {
+
+    public static EditCursoDto of(Curso dto) {
+        return new EditCursoDto(dto.getNombre(), dto.getHorasEmpresa());
+    }
+}
--- conecta/src/main/java/com/salesianostriana/dam/conecta/service/CursoService.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/service/CursoService.java
@@ -1,4 +1,42 @@
 package com.salesianostriana.dam.conecta.service;
 
+import com.salesianostriana.dam.conecta.dtos.EditCursoDto;
+import com.salesianostriana.dam.conecta.model.Curso;
+import com.salesianostriana.dam.conecta.repository.CursoRepo;
+import com.salesianostriana.dam.conecta.repository.ProfesorRepo;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CursoService {
+
+    //Repositorios utilizados en el servicio
+    private CursoRepo cursoR;
+    private ProfesorRepo profesorR;
+
+
+    public List<Curso> findAllCursos() {
+        List<Curso> cursos = cursoR.findAll();
+        if(cursos.isEmpty()){
+            throw new EntityNotFoundException("No se han encontrado cursos ");
+        }
+        return cursos;
+    }
+    //Buscar por id CURSOS
+    public Curso findCursoById(Long id) {
+        Optional<Curso> cursosOp= cursoR.findById(id);
+        if(cursosOp.isEmpty()){
+            throw new EntityNotFoundException("No se han encontrado cursos cone ese id ");
+        }
+        return cursosOp.get();
+    }
+    public Curso saveCurso(EditCursoDto cursdto) {
+        return cursoR.save(Curso.builder()
+                .nombre(cursdto.nombre()).horasEmpresa(cursdto.horasEmpresa()).build());
+    }
 }
