a7402db0d1efd16d4313668e86dd6804cb6614a9
ppsegur <pepesegurarodriguez@gmail.com>
2025-01-29 13:36:54 +0100

    Editar profesor sin tener en cuenta las asociaciones

--- conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/EditCursoDto.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/EditCursoDto.java
@@ -6,6 +6,6 @@ import com.salesianostriana.dam.conecta.model.Curso;
 public record EditCursoDto(String nombre, int horasEmpresa) {
 
     public static EditCursoDto of(Curso dto) {
-        return new EditCursoDto(dto.getNombre(), dto.getHorasEmpresa());
+        return new EditCursoDto(dto.getNombre(), dto.getHorasEmpresa()/*, EditCursoDto.of(dto.getProfesores())*/);
     }
 }
