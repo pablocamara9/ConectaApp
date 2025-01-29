322de8cb4a88532d3ed7f672288842891b89226b
ppsegur <pepesegurarodriguez@gmail.com>
2025-01-29 11:50:05 +0100

    Creación de un profesor conseguido sin asociaciones

--- conecta/src/main/java/com/salesianostriana/dam/conecta/model/Profesor.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/model/Profesor.java
@@ -43,6 +43,20 @@ public class Profesor extends Persona{
         curso.getProfesores().remove(this);
 
     }
+    //Equals & HasCode
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Profesor profesor = (Profesor) o;
+        return getId() != null && Objects.equals(getId(), profesor.getId());
+    }
 
-
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
