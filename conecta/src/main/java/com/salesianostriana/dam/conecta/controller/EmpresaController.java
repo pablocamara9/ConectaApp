1c83bebaa55998f1f7521184fc9647b99ae456a9
Pablo <pcamaragar@gmail.com>
2025-01-30 22:06:53 +0100

    Terminado método POST en EmpresaController. También pequeño cambio en EmpresaService y en el DTO para agregar empresas.

--- conecta/src/main/java/com/salesianostriana/dam/conecta/controller/EmpresaController.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/controller/EmpresaController.java
@@ -1,15 +1,27 @@
 package com.salesianostriana.dam.conecta.controller;
 
+import com.salesianostriana.dam.conecta.dtos.EditEmpresaDto;
+import com.salesianostriana.dam.conecta.model.Empresa;
 import com.salesianostriana.dam.conecta.service.EmpresaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/empresa")
+@RequestMapping("/empresa/")
 public class EmpresaController {
 
     private final EmpresaService empresaService;
+
+    @PostMapping
+    public ResponseEntity<Empresa> addEmpresa(@RequestBody EditEmpresaDto empresa) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(empresaService.save(empresa));
+    }
 
 }
