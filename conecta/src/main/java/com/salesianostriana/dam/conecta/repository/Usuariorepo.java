714be797bf83ec4b0453679d9aa32d4142c8bfed
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-01 18:25:18 +0100

    Creación del repositorio de Usuario

---
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/repository/Usuariorepo.java
@@ -0,0 +1,6 @@
+package com.salesianostriana.dam.conecta.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Usuariorepo extends JpaRepository<Usuariorepo, Long> {
+}
