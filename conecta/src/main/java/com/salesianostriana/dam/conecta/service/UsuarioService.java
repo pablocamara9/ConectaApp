810e6ceeaa5379ebb0de9057777b42a5dffc1f0d
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-03 20:40:33 +0100

    Creados los métodos buscar y listar usuarios en el servicio

--- conecta/src/main/java/com/salesianostriana/dam/conecta/service/UsuarioService.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/service/UsuarioService.java
@@ -1,9 +1,14 @@
 package com.salesianostriana.dam.conecta.service;
 
+import com.salesianostriana.dam.conecta.model.Profesor;
 import com.salesianostriana.dam.conecta.model.Usuario;
 import com.salesianostriana.dam.conecta.repository.Usuariorepo;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,23 @@ public class UsuarioService {
     //Guardamos el usuario
     public Usuario saveUsuario(Usuario usuario) {
         return usuariorepo.save(usuario);
+    }
+
+    //Buscar todos los usuarios
+    public List<Usuario> findallUsuarios() {
+        List<Usuario> usuarios = usuariorepo.findAll();
+        if(usuarios.isEmpty())
+            throw new EntityNotFoundException("No existen usuarios");
+        return usuarios;
+    }
+
+    //Buscar por id Usuarios
+    public Profesor findUsuarioById(Long id) {
+        Optional<Usuario> usuario = usuariorepo.findById(id);
+        if(usuario.isEmpty()){
+            throw new EntityNotFoundException("No se han encontrado usuario cone ese id ");
+        }
+        return usuario.get().getProfesor();
     }
 
 }
