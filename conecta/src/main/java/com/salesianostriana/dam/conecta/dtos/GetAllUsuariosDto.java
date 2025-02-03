bdfb8e2f28c54e8d59f0d05c374594e39f3a43e5
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-03 20:50:15 +0100

    Subida del dto del getAll Usuarios completado

--- conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/GetAllUsuariosDto.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/GetAllUsuariosDto.java
@@ -1,4 +1,17 @@
 package com.salesianostriana.dam.conecta.dtos;
 
-public record GetAllUsuariosDto() {
+import com.salesianostriana.dam.conecta.model.Usuario;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record GetAllUsuariosDto(
+        List<GetUsuarioDto> listadoDeUsuarios
+) {
+    public static GetAllUsuariosDto getAllUsuariosDto(List<Usuario> listadoDeUsuariosSinProcesar) {
+        return GetAllUsuariosDto.builder()
+                .listadoDeUsuarios(listadoDeUsuariosSinProcesar.stream().map(GetUsuarioDto::of).toList())
+                .build();
+    }
 }
