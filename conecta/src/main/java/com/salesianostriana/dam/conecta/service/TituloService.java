ed6d91b837e6f01136b8976a60589c93f355f628
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-04 10:45:14 +0100

    Creaciñón de los métodos buscar y listar en el sevicio de títulos

--- conecta/src/main/java/com/salesianostriana/dam/conecta/service/TituloService.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/service/TituloService.java
@@ -1,9 +1,14 @@
 package com.salesianostriana.dam.conecta.service;
 
+import com.salesianostriana.dam.conecta.model.Profesor;
 import com.salesianostriana.dam.conecta.model.Titulo;
 import com.salesianostriana.dam.conecta.repository.TituloRepo;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,22 @@ public class TituloService {
     //Guardar el título
     public Titulo saveTitulo(Titulo titulo) {
         return repo.save(titulo);
+    }
+    //Buscar todos los titulos
+    public List<Titulo> findallTitulos() {
+        List<Titulo> titulos = repo.findAll();
+        if(titulos.isEmpty())
+            throw new EntityNotFoundException("No existen titulos");
+        return titulos;
+    }
+
+    //Buscar por id títulos
+    public Titulo findTituloById(Long id) {
+        Optional<Titulo> titulo = repo.findById(id);
+        if(titulo.isEmpty()){
+            throw new EntityNotFoundException("No se han encontrado titulo cone ese id ");
+        }
+        return titulo.get();
     }
 
 }
