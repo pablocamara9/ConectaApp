c24144c898502b30dbb02a1f0a476ae4042331d1
Pablo <pcamaragar@gmail.com>
2025-02-04 23:43:57 +0100

    Cambio en GetDemandaDto

--- conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/GetDemandaDto.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/GetDemandaDto.java
@@ -3,13 +3,13 @@ package com.salesianostriana.dam.conecta.dtos;
 import com.salesianostriana.dam.conecta.model.Demanda;
 import com.salesianostriana.dam.conecta.model.Empresa;
 
-public record GetDemandaDto(int cantidadAlumnos, String requisitos, Empresa empresa) {
+public record GetDemandaDto(int cantidadAlumnos, String requisitos, GetEmpresaDto empresa) {
 
     public static GetDemandaDto of(Demanda dto) {
         return new GetDemandaDto(
                 dto.getCantidadAlumnos(),
                 dto.getRequisitos(),
-                dto.getEmpresa()
+                GetEmpresaDto.of(dto.getEmpresa())
         );
     }
 }
