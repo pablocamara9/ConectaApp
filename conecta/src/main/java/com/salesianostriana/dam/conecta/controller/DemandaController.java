edefbf22dd7fd2ca4a455af58849512938462b07
Pablo <camara.gapab23@triana.salesianos.edu>
2025-02-04 14:30:22 +0100

    Agregado metodo FindAll (GET) en el controlador. Solucionar recusion infinita al hacer la peticion.

--- conecta/src/main/java/com/salesianostriana/dam/conecta/controller/DemandaController.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/controller/DemandaController.java
@@ -1,9 +1,14 @@
 package com.salesianostriana.dam.conecta.controller;
 
+import com.salesianostriana.dam.conecta.dtos.GetDemandaDto;
+import com.salesianostriana.dam.conecta.model.Demanda;
 import com.salesianostriana.dam.conecta.service.DemandaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,5 +17,9 @@ public class DemandaController {
 
     private final DemandaService demandaService;
 
+    @GetMapping
+    public List<GetDemandaDto> findAll() {
+        return demandaService.findAll().stream().map(GetDemandaDto::of).toList();
+    }
 
 }
