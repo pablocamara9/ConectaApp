23d0212b26790a418f08248aa2eb76d62aa210f4
Pablo <camara.gapab23@triana.salesianos.edu>
2025-02-05 10:41:52 +0100

    Cambio en un DTO

--- conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/GetFamiliaProfesionalDto.java
+++ conecta/src/main/java/com/salesianostriana/dam/conecta/dtos/GetFamiliaProfesionalDto.java
@@ -1,16 +1,17 @@
 package com.salesianostriana.dam.conecta.dtos;
 
-import com.salesianostriana.dam.conecta.model.Empresa;
 import com.salesianostriana.dam.conecta.model.FamiliaProfesional;
 
 import java.util.List;
 
-public record GetFamiliaProfesionalDto(String nombre, List<Empresa> empresasAsociadas) {
+public record GetFamiliaProfesionalDto(String nombre, List<GetEmpresaDto> empresasAsociadas) {
 
     public static GetFamiliaProfesionalDto of(FamiliaProfesional dto) {
         return new GetFamiliaProfesionalDto(
                 dto.getNombre(),
-                dto.getEmpresasRelacionadas()
+                dto.getEmpresasRelacionadas().stream().map(GetEmpresaDto::of).toList()
+
+                //dto.getTrabajadores().stream().map(GetTrabajadorDto::of).toList()
         );
     }
 }
